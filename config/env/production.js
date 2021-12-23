cf8f2f7e678e9eb863678f611f23f1efd368abd0
Kenny Cheng <kennycheng@comp.hkbu.edu.hk>
2021-12-23 17:09:15 +0800

    Update production.js

--- config/env/production.js
+++ config/env/production.js
@@ -149,7 +149,7 @@ module.exports = {
     ***************************************************************************/
     cors: {
       allowOrigins: [
-        'https://joe061005.herokuapp.com/'
+        'https://joe061005.herokuapp.com'
       ]
     },
 
@@ -251,7 +251,7 @@ module.exports = {
     *                                                                          *
     ***************************************************************************/
     onlyAllowOrigins: [
-      'https://joe061005.herokuapp.com/'
+      'https://joe061005.herokuapp.com'
     ],
 
 
